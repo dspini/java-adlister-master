2bc258ed292c974efc79bc56e1497f168bc2dd8b
Donald Spini <donald.spini68@gmail.com>
2020-09-22 15:53:08 -0500

    adlister

---
+++ java-adlister/src/main/java/dao/Config.java
@@ -0,0 +1,13 @@
+package dao;
+
+class Config {
+    public String getUrl() {
+        return "jdbc:mysql://localhost/database_name?serverTimezone=UTC&useSSL=false";
+    }
+    public String getUser() {
+        return "someuser";
+    }
+    public String getPassword() {
+        return "strongpassword";
+    }
+}
